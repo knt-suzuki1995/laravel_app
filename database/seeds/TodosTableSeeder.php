a33122344015d2a5cb81c7788f76e11ed471dc67
knt-suzuki1995 <k3wxyga@gmail.com>
2022-05-11 16:19:42 +0900

    create: laravel-app

---
+++ database/seeds/TodosTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todos')->insert([
+            [
+                'content' => '開発環境を構築する',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'content' => 'Laravelをインストールする',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
